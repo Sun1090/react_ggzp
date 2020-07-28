89900b76b6eebf963387979033160b1e2b62bf0a
Sun1090 <1090964491@qq.com>
2020-07-28 08:36:57 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+/* 
+  ??JS
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { HashRouter, Switch, Route } from "react-router-dom";
+
+import store from "./redux/store";
+import Login from "./containers/login/login";
+import Register from "./containers/register/register";
+import Main from "./containers/main/main";
+import "./assets/css/index.less";
+
+ReactDOM.render(
+  <Provider>
+    <HashRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route component={Main} />
+      </Switch>
+    </HashRouter>
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/redux/action-types.js
@@ -0,0 +1,9 @@
+/* 
+  包含所有action的type常量模块
+*/
+
+// 验证成功
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+
+//  请求出错
+export const ERROR_MSG = "ERROR_MSG";
